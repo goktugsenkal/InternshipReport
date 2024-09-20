0da495f69ea5bf113af8254aa01f133430fcea0c
Goktug Ubuntu <goktugsenkal1881@gmail.com>
2024-09-20 15:05:55 +0300

    Add migration InitialCreate

---
+++ Infrastructure/Migrations/20240920110558_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InternshipReports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    RegularShift = table.Column<int>(type: "integer", nullable: false),
+                    LeaveDays = table.Column<List<DateTime>>(type: "timestamp with time zone[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InternshipReports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReportEntry",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    InternshipReportId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportEntry", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReportEntry_InternshipReports_InternshipReportId",
+                        column: x => x.InternshipReportId,
+                        principalTable: "InternshipReports",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReportEntry_InternshipReportId",
+                table: "ReportEntry",
+                column: "InternshipReportId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReportEntry");
+
+            migrationBuilder.DropTable(
+                name: "InternshipReports");
+        }
+    }
+}
