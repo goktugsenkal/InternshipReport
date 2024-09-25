4fac40de49ec2d5ec45aec661d645f3e55ff27b9
Goktug Ubuntu <goktugsenkal1881@gmail.com>
2024-09-25 21:32:28 +0300

    Update ReportEntity to have nav property Report

--- Core/Entities/ReportEntry.cs
+++ Core/Entities/ReportEntry.cs
@@ -3,5 +3,6 @@ namespace Core.Entities;
 public class ReportEntry : BaseEntity
 {
     public string Content { get; set; } = string.Empty;
+    public required InternshipReport InternshipReport { get; set; }
     public int InternshipReportId { get; set; }
 }
